d9f92e0dbc88174ab5c87c75fbbdfc50423bf08c
smithwebtek <brad@smithwebtek.com>
2018-06-19 18:32:05 -0400

    Update songs controller

--- app/controllers/api/mp3s_controller.rb
+++ app/controllers/api/mp3s_controller.rb
@@ -1,2 +1,54 @@
 class Api::Mp3sController < ApplicationController
+	before_action :set_mp3, only: [:show, :update, :destroy, :resources]
+	
+	def index
+		@mp3s = Mp3.all
+		render json: @mp3s
+	end
+
+	def show
+		render json: @mp3
+	end
+
+	def create
+		@mp3 = Mp3.new(mp3_params)
+		if @mp3.save
+			render json: @mp3
+		else
+			render json: { errors: { message: 'mp3 NOT created' }}
+		end
+	end
+
+	def update
+		@mp3.update(mp3_params)
+		if @mp3.save
+			render json: @mp3
+		else
+			render json: { errors: { message: 'mp3 NOT updated' }}
+		end
+	end
+
+	def destroy
+		@mp3.delete
+		render json: { message: 'mp3 deleted' }
+	end
+	
+	def resources
+		@resources = @mp3.resources
+		if @resources
+			render json: @resources
+		else
+			render json: { errors: { message: 'mp3 resources NOT found' }}
+		end
+	end
+
+	private
+	def set_mp3
+		@mp3 = Mp3.find_by_id(params[:id])
+		end
+	def mp3_params
+		params.require(:mp3).permit(:name)
+	end
+
 end
+	
--- app/controllers/api/songs_controller.rb
+++ app/controllers/api/songs_controller.rb
@@ -1,2 +1,53 @@
 class Api::SongsController < ApplicationController
+	before_action :set_song, only: [:show, :update, :destroy, :resources]
+	
+	def index
+		@songs = Song.all
+		render json: @songs
+	end
+
+	def show
+		render json: @song
+	end
+
+	def create
+		@song = Song.new(song_params)
+		if @song.save
+			render json: @song
+		else
+			render json: { errors: { message: 'song NOT created' }}
+		end
+	end
+
+	def update
+		@song.update(song_params)
+		if @song.save
+			render json: @song
+		else
+			render json: { errors: { message: 'song NOT updated' }}
+		end
+	end
+
+	def destroy
+		@song.delete
+		render json: { message: 'song deleted' }
+	end
+	
+	def resources
+		@resources = @song.resources
+		if @resources
+			render json: @resources
+		else
+			render json: { errors: { message: 'song resources NOT found' }}
+		end
+	end
+
+	private
+	def set_song
+		@song = Song.find_by_id(params[:id])
+		end
+	def song_params
+		params.require(:song).permit(:name)
+	end
+
 end
